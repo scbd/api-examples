951395fcd4bba01269cd456e716276fe48961ac9
Blaise Fonseca <blaisefonseca@hotmail.com>
2018-11-30 10:54:23 +0530

    Update EContact.cs

--- schema-definitions/EContact.cs
+++ schema-definitions/EContact.cs
@@ -1,14 +1,17 @@
+ // Copyright (c) 2001-2016 Secretariat of the Convention on Biological Diversity
+// This source file is subject to the New BSD license that is bundled with this package in the file LICENSE.txt
+
 public class EContact :  EContactBase
 {
-  [JsonProperty(Required=Required.Always)]
-  public EHeader	header					{ get; set; }
+      [JsonProperty(Required=Required.Always)]
+      public EHeader	header					{ get; set; }
 
       public lstring  relevantInformation         { get; set; }
       public ELink[]  relevantDocuments           { get; set; }
       public string   notes                       { get; set; }
 
       public string   type                        { get; set; } // "organization" || "person" -> default "person"
-  [Term("countries")]
+      [Term("countries")]
       public EReference government                { get; set; }
       public EReference contactOrganization       { get; set; }
 
